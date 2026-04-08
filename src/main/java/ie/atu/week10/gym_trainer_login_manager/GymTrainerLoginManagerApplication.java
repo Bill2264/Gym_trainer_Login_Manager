aeccebcc5ebe5ae7e8a237b494e533956f14727a
Billy Murphy <G00465240@atu.ie>
2026-04-08 23:05:31 +0100

    Initial commit

---
+++ src/main/java/ie/atu/week10/gym_trainer_login_manager/GymTrainerLoginManagerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week10.gym_trainer_login_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrainerLoginManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTrainerLoginManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week10/gym_trainer_login_manager/GymTrainerLoginManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week10.gym_trainer_login_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrainerLoginManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
